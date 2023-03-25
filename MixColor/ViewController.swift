a01d633e7a8dab5d05f67354bb0f8ae237d6d7a7
ALEXANDR SINYUTIN <shura.sinyutin@icloud.com>
2023-03-25 23:52:35 +0300

    add mixing colors

--- MixColor/ViewController.swift
+++ MixColor/ViewController.swift
@@ -7,13 +7,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
 
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var paletteView: UIView!
+    @IBOutlet weak var currentValueLebelRed: UILabel!
+    @IBOutlet weak var currentValueLabelGreen: UILabel!
+    @IBOutlet weak var currentValueLabelBlue: UILabel!
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    private func setColors () {
+        paletteView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        paletteView.layer.cornerRadius = 10
     }
-
-
+    
+    @IBAction func redSliderAction() {
+        currentValueLebelRed.text = redSlider.value.formatted(.number.precision(.fractionLength(2)))
+       setColors()
+    }
+    @IBAction func greenSliderAction() {
+        currentValueLabelGreen.text = greenSlider.value.formatted(.number.precision(.fractionLength(2)))
+        setColors()
+    }
+    @IBAction func blueSliderAction() {
+        currentValueLabelBlue.text = blueSlider.value.formatted(.number.precision(.fractionLength(2)))
+        setColors()
+        
+    }
 }
-
